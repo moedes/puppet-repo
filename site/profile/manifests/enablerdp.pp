33cd3277c5385a5c9c1f099dd7dcd891ff331be9
moedes <jmozes@gmail.com>
2017-09-15 09:34:16 -0600

    First attempt at enabling RDP via Powershell for base windows

---
+++ site/profile/manifests/enablerdp.pp
@@ -0,0 +1,6 @@
+class profile::enablerdp {
+    exec {
+	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0'
+        provider => powershell
+    }
+}
