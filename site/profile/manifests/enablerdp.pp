dc5b8d8c1ed54585d0eee8218b5b1d6bd41619d6
moedes <jmozes@gmail.com>
2017-09-15 09:50:28 -0600

    added title to exec command

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,5 +1,5 @@
 class profile::enablerdp {
-    exec {
+    exec {'enable-rdp':
 	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0',
         provider => powershell,
     }
