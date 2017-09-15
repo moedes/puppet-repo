9f42d41383147c8379af9d7c4a2cbcb20945353f
moedes <jmozes@gmail.com>
2017-09-15 09:48:03 -0600

    Corrected syntax error

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,6 +1,6 @@
 class profile::enablerdp {
     exec {
-	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0'
-        provider => powershell
+	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0',
+        provider => powershell,
     }
 }
