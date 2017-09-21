0ff692ad4e2b28901b4f14cf8d9fc5f1f9fa9be6
moedes <jmozes@gmail.com>
2017-09-21 12:58:02 -0600

    Set HKLM Value to 0

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,6 +1,6 @@
 class profile::enablerdp {
     exec {'enable-rdp':
-	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0',
+	command => 'New-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSetControl\Terminal Server’ -Name fDenyTSConnections -Value 0',
         provider => powershell,
     }
 }
