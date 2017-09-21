196e7e4af491f5cd11c62a9c5f14468ea1f3c739
moedes <jmozes@gmail.com>
2017-09-21 13:29:46 -0600

    Added Enable Firewall Rule

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,6 +1,7 @@
 class profile::enablerdp {
     exec {'enable-rdp':
 	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSet\Control\Terminal Server’ -Name fDenyTSConnections -Value 0',
-        provider => powershell,
+        command => 'Enable-NetFirewallRule -DisplayGroup 'Remote Desktop'
+	provider => powershell,
     }
 }
