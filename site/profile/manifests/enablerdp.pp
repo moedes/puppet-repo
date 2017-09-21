ce00bb0f6bc1666845dd11f123212b8dbd21ab0d
moedes <jmozes@gmail.com>
2017-09-21 13:17:09 -0600

    Change New-Item Property to Set-ItemProperty

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,6 +1,6 @@
 class profile::enablerdp {
     exec {'enable-rdp':
-	command => 'New-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSet\Control\Terminal Server’ -Name fDenyTSConnections -Value 0',
+	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSet\Control\Terminal Server’ -Name fDenyTSConnections -Value 0',
         provider => powershell,
     }
 }
