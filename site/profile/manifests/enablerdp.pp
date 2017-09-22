d18676957bc7d3c87e63e5e90159e435f6f1ff3b
moedes <jmozes50@gmail.com>
2017-09-22 10:40:06 -0600

    Remove bad syntax from Enable Netfirewall rule

--- site/profile/manifests/enablerdp.pp
+++ site/profile/manifests/enablerdp.pp
@@ -1,7 +1,6 @@
-class profile::enablerdp {
+﻿class profile::enablerdp {
     exec {'enable-rdp':
 	command => 'Set-ItemProperty -Path ‘HKLM:SYSTEM\CurrentControlSet\Control\Terminal Server’ -Name fDenyTSConnections -Value 0',
-        command => 'Enable-NetFirewallRule -DisplayGroup 'Remote Desktop'',
 	provider => powershell,
     }
 }
